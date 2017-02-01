9f2be036e0e7bab9641b4061cd602c943047b266
CaptainTheDelta <lesecq.damien@gmail.com>
2017-02-01 17:56:03 +0100

    Rectification TP1 ex6

--- Info/TP/TP1 - Ex06.py
+++ Info/TP/TP1 - Ex06.py
@@ -3,10 +3,10 @@
 print("TP1 - Ex6")
 
 # Consigne :
-print("Ouvrez le fichier TP1.py et exécutez le.\n\n")
+print("\nOuvrez le fichier TP1.py et exécutez le.\n\n")
 
 # Exercice :
 # Bon ben là, débrouillez vous, hein ?
 
 # Conclusion :
-print("Le fichier est exécuté dans Spyder")
+print("\nLe fichier est exécuté dans Spyder")
